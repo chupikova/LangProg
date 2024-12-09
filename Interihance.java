b111f951c78e4727edc49d307284d5c9bf5d9507
chupikova <mashachupikova@yandex.ru>
2024-12-09 15:36:47 +0700

    Добавили демонстрацию полиморфизма и класс цветной треугольник

--- Interihance.java
+++ Interihance.java
@@ -43,6 +43,10 @@ class TwoDShape {
 		return name;
 	}
 	*/
+	double area() {
+		Sytem.out.println("Метод должен быть переопределён!");
+		return 0.0;
+	}
 }
 class Triangle extends TwoDShape {
 	String style;
@@ -68,7 +72,18 @@ class Triangle extends TwoDShape {
 	}
 	*/
 }
+class ColorTriangle extends Triangle {
+	String color;
+	ColorTriangle(String c, String s, double w, double h) {
+		super(s,w,h); //Вызов конструктора Triangle();
+		color = c;
+		name = "Цветной треугольник";
+	}
+	void showColor() {
+		System.out.println("Цвет: " + color);
+	}
 
+}
 class Rectangle extends TwoDShape {
 	Rectangle(String o, double w, double h) {
 		super(w, h); //вызов конструктора суперкласса
@@ -89,7 +104,7 @@ class Interihance {
 	public static void main(String[] args) {
 	TwoDShape fig1 = new TwoDShape(20.0, 30.0);
 	Triangle t1 = new Triangle("Закрашенный", 4.5, 7.0);
-	Triangle t2 = new Triangle("Контурный", 3.5, 8.0);
+	Triangle t2 = new ColorTriangle("Синий","Контурный", 3.5, 8.0);
 	Rectangle r1 = new Rectangle("Сплошная линия", 5.0, 5.0);
 	Rectangle r2 = new Rectangle("Пунктирная линия", 5.0, 7.0);
 	System.out.println("Информация об объектах");
@@ -108,13 +123,47 @@ class Interihance {
 	System.out.println(t1.i);
 	System.out.println(t1.showSuperI());
 
+	//Совместимость переменных родственных классов
+	//Переменная суперкласс может ссылаться на объект любого подкласса
+	//
+	System.out.println();
+	System.out.println("Ширина двумерной фигуры fig1: " + fig1.getWidth());
+	//При указании на объект суперкласса выполняется метод суперкласса
+	fig1.area();
+	System.out.println("Имя фигуры: " + fig1.name);
+
+
+	fig1 = t1;
+	System.out.println("Ширина треугольника, на который ссылается fig1: " +fig1.getWidth());
+	//При указании на объект подкласса "Треугольник" выполняется вычисление площади для треугольника
+	System.out.println("Площадь треугольника: "+ fig1.area());
+	System.out.println("Имя фигуры: " + fig1.name);
+
+	fig1 = r1;
+	System.out.println("Ширина прямоугольника, на который ссылается fig1: "fig1.getWidth());
+	//При указании на объект подкласса "Прямоугольник" выполняется вычисление площади для прямоугольника
+	System.out.println("Площадь прямоугольника: " + fig1.area()); 
+	System.out.println("Имя фигуры: " + fig1.name);
+
+	fig1 = t2;
+	t2.showColor();
+	System.out.println("Площадь цветного треугольника: " + fig1.area());
+	System.out.println("Имя фигуры: " + fig1.name);
+
+
+        //Доступ к членам подкласса для ссылочных переменных суперкласса закрыт:
+	//System.out.println(fig1.style);
+
+
+
+
 	//Проверка доступности переменных
 	/*
 	System.out.println(fig1.width);
 	System.out.println(t1.width); //Подкласс имеет доступ к переменным и методам суперкласса
 	System.out.println(fig1.style); //Суперкласс не имеет доступа к переменным, описанным в подклассе
 	*/
-
+	
 
 	}
 }
